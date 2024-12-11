e74aa3c3e56d123c0370a406fffc214f1295723f
h-pozuelo <122695697+h-pozuelo@users.noreply.github.com>
2024-12-11 20:56:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+morgan.token("body", (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/", (request, response) => {
+  response.status(200).send("<h1>Hello World!</h1>");
+});
+
+app.get("/info", (request, response) => {
+  response.status(200).send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+       <p>${new Date()}</p>`
+  );
+});
+
+app.get("/api/persons", (request, response) => {
+  response.status(200).json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) return response.status(200).json(person);
+
+  response.statusMessage = "Note Not Found";
+  response.status(404).end();
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  let id = 0;
+
+  do {
+    id = getRandomIntInclusive(1, 100);
+  } while (persons.find((person) => person.id === id));
+
+  return id;
+};
+
+const getRandomIntInclusive = (min, max) => {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number)
+    return response.status(400).json({ error: "name and number are required" });
+
+  if (
+    persons.find(
+      (person) => person.name.toLowerCase() === body.name.toLowerCase()
+    )
+  )
+    return response.status(400).json({ error: "name must be unique" });
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.status(200).json(person);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
