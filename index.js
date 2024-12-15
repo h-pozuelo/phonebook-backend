b00d6933b285d0067d6293f06e33a3aa3be9a373
h-pozuelo <122695697+h-pozuelo@users.noreply.github.com>
2024-12-15 14:07:55 +0100

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require("./models/persons");
 
 app.use(cors());
 
@@ -12,6 +14,18 @@ app.use(express.json());
 morgan.token("body", (request, response) => {
   return JSON.stringify(request.body);
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === "CastError") {
+    response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    response.status(400).send({ error: error.message });
+  } else {
+    next(error);
+  }
+};
 
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
@@ -45,31 +59,30 @@ app.get("/", (request, response) => {
 });
 
 app.get("/info", (request, response) => {
-  response.status(200).send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-       <p>${new Date()}</p>`
-  );
+  Person.find({}).then((persons) => {
+    response.status(200).send(
+      `<p>Phonebook has info for ${persons.length} people</p>
+         <p>${new Date()}</p>`
+    );
+  });
 });
 
 app.get("/api/persons", (request, response) => {
-  response.status(200).json(persons);
+  Person.find({}).then((persons) => response.status(200).json(persons));
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) return response.status(200).json(person);
-
-  response.statusMessage = "Note Not Found";
-  response.status(404).end();
+  const id = request.params.id;
+  Person.findById(id).then((person) => response.status(200).json(person));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  const id = request.params.id;
+  Person.findByIdAndDelete(id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
@@ -88,28 +101,47 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number)
     return response.status(400).json({ error: "name and number are required" });
 
-  if (
-    persons.find(
-      (person) => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-  )
-    return response.status(400).json({ error: "name must be unique" });
+  // if (
+  //   persons.find(
+  //     (person) => person.name.toLowerCase() === body.name.toLowerCase()
+  //   )
+  // )
+  //   return response.status(400).json({ error: "name must be unique" });
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => response.status(200).json(savedPerson))
+    .catch((error) => next(error));
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateId(),
   };
 
-  persons = persons.concat(person);
-
-  response.status(200).json(person);
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
+    .then((updatedPerson) => {
+      response.status(200).json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (request, response) => {
@@ -117,7 +149,9 @@ const unknownEndpoint = (request, response) => {
 };
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
